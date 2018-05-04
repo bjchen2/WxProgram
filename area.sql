b49820effe98acf8681b0f66562bf95f8e801be8
MsnYa <1210279050@qq.com>
2018-05-04 13:28:40 +0800

    Update area.sql

--- area.sql
+++ area.sql
@@ -3,7 +3,7 @@ CREATE TABLE `area` (
   `name` varchar(200) NOT NULL,
   `priority` int(11) NOT NULL DEFAULT '0' COMMENT '权值',
   `create_time` bigint(20) DEFAULT NULL COMMENT '创建时间戳',
-  `last_edit_time` bigint(20) DEFAULT NULL COMMENT '最后修改时间',
+  `last_edit_time` bigint(20) DEFAULT NULL COMMENT '最后修改时间戳',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8
